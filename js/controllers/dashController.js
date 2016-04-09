626cb0eed51abba72458aa765d69befec0bdf8dd
Nicolai <nkcryer@gmail.com>
2016-04-09 11:54:14 +0200

    Basic sidebar toggle

--- js/controllers/dashController.js
+++ js/controllers/dashController.js
@@ -1,19 +1,27 @@
-app.controller('dashController', function($scope, $state, Auth){
+app.controller('dashController', function($mdComponentRegistry, $mdUtil, $mdSidenav, $scope, $state, Auth){
 
   var self = $scope;
 
   self.logout = function(){
     Auth.logout();
     $state.transitionTo('login');
+  };
 
-  
+  $mdComponentRegistry.when('left').then(function(component){
+    component.toggle();
+  });
+
   $scope.toggleLeft = buildToggler('left');
 
-function buildToggler(navID) {
-    var debounceFn = $mdUtil.debounce(function () {
-        $mdSidenav(navID)
-            .toggle()
-    }, 100);
+  function buildToggler(navID) {
+    var debounceFn = $mdUtil.debounce(function(){
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          console.log("toggled");
+        });
+    },300);
     return debounceFn;
-   };
-}}]);
+  };
+
+});
